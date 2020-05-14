cd8fad5d166c685afa4f2ea1e8f18490aab00a07
asif <asu9421@gmail.com>
2020-05-14 21:03:09 +0530

    second commit

--- src/test/java/wiremock/TestClass2.java
+++ src/test/java/wiremock/TestClass2.java
@@ -1,57 +1,53 @@
 package wiremock;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import io.restassured.response.Response;
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static io.restassured.RestAssured.given;
 
+@Log4j2
 public class TestClass2 {
 
     Stubs stubs;
     MockBase mockBase;
-    WireMockServer wireMockServer;
 
     @BeforeClass
     public void setUp() {
         mockBase = new MockBase();
         mockBase.turnOffWiremockLogging();
-        wireMockServer = new WireMockServer(wireMockConfig().port(2345));
-        wireMockServer.start();
-        System.out.println(wireMockServer.port());
+        mockBase.startWireMockServerOnThisPort(2345);
         stubs = new Stubs();
     }
 
     @AfterClass
     public void tearDown() {
+        mockBase.removeAllStub();
         mockBase.stopWireMockServer();
         mockBase.closePrintStream();
+
     }
 
     @Test
     public void testForSpecificPort() {
-        StubMapping stubMapping = wireMockServer.stubFor(get(urlEqualTo("/emp/v2"))
+        mockBase.getWireMockServer().stubFor(get(urlEqualTo("/emp/v2"))
                 .willReturn(aResponse()
-                        .withBody("Employee 1\nEmployee2\n")
+                        .withBody("Employee 001 \nEmployee 002\n")
                         .withStatus(200)
                 ));
         Response response = given().spec(mockBase.setRALogFilter())
-                .baseUri("http://localhost:2345/")
-                .basePath("/emp")
                 .port(2345)
                 .when()
-                .get("v2")
+                .get("/emp/v2")
                 .then()
                 .extract()
                 .response();
         System.out.println(response.getStatusLine());
         System.out.println(response.asString());
-        wireMockServer.removeStub(stubMapping);
+
     }
 
 
